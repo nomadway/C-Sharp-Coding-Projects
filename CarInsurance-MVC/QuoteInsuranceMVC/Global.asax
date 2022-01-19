2e872487039d319ad0eace88bb95add720244448
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 23:32:51 -0700

    commit-draft-1

---
+++ CarInsurance-MVC/QuoteInsuranceMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuoteInsuranceMVC.MvcApplication" Language="C#" %>
---
+++ CarInsurance-MVC/QuoteInsuranceMVC/ViewModels/QuoteVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuoteInsuranceMVC.ViewModels
+{
+    public class QuoteVm
+    {
+        public double Quote { get; set; }
+
+
+
+
+    }
+}
